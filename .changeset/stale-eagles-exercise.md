83fc2025fc0667c6bfd8e69a09881e5e4eff55f9
Simon <wsj_simon@163.com>
2022-01-05 14:03:57 +0800

    update main

---
+++ .changeset/stale-eagles-exercise.md
@@ -0,0 +1,5 @@
+---
+"@flypkg/main": major
+---
+
+update initial
--- packages/main/lib/index.js
+++ packages/main/lib/index.js
@@ -1,7 +1,6 @@
-import { logMessage, runSubA } from '@flypkg/suba'
+import { runSubA } from '@flypkg/suba'
 import pkg from '../package.json'
 
 export function init () {
-  logMessage('main', pkg.version, '在 main 中执行，当前版本：0.1.0')
-  runSubA()
+  runSubA(`在 main 中执行，main 版本为：${pkg.version}`)
 }
